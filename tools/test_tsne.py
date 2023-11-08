3cef02e1847784cba9f2aaaa71810aff9a7cc784
puneet13150 <puneet13150@iiitd.ac.in>
2023-11-08 13:34:13 -0700

    updating plotting in test_tsne

--- tools/test_tsne.py
+++ tools/test_tsne.py
@@ -95,31 +95,33 @@ for row in metadata.iterrows():
         # k = 1000
         # sampled_nodes = random.sample(list(self.dat.nodes), k)
         # dat = dat.subgraph(sampled_nodes)
-        draw = True
+        draw = False
         print('1')
-        plt.figure()
+        
+        
+        print('2')
+        # k = 1000
+        # sampled_nodes = random.sample(list(dat.nodes), k)
+        # dat = dat.subgraph(sampled_nodes)
+        # pdb.set_trace()
+        sampled_graph = dat
+        # pos = nx.spring_layout(sampled_graph)#, pos=positions)
+        # pos = nx.nx_pydot.graphviz_layout(sampled_graph)
+        ''' NEATO'''
+        fil_pos = open('poses_test.pickle', 'rb')
+        pos = pickle.load(fil_pos)
+        fil_pos.close()
+        print('3')
+        sizes_ = [10.0*a for a in nx.get_node_attributes(sampled_graph, 'sz').values()]
+        positions = [a for a in nx.get_node_attributes(sampled_graph, 'x').values()]
+        colors_ = [a for a in nx.get_node_attributes(sampled_graph, 'colors').values()]
+        alphas = [0.2 if a=='r' else 1.0 for a in colors_]
+        sizes_ = [s*0.1 if r=='r' else s for s,r in zip(sizes_, colors_)]
+        sizes_ = [s*10.0 if r=='g' else s for s,r in zip(sizes_, colors_)]
+        # plt.figure(0)
+        # pdb.set_trace()
         if draw:
-            print('2')
-            # k = 1000
-            # sampled_nodes = random.sample(list(dat.nodes), k)
-            # dat = dat.subgraph(sampled_nodes)
-            # pdb.set_trace()
-            sampled_graph = dat
-            # pos = nx.spring_layout(sampled_graph)#, pos=positions)
-            # pos = nx.nx_pydot.graphviz_layout(sampled_graph)
-            ''' NEATO'''
-            fil_pos = open('poses_test.pickle', 'rb')
-            pos = pickle.load(fil_pos)
-            fil_pos.close()
-            print('3')
-            sizes_ = [10.0*a for a in nx.get_node_attributes(sampled_graph, 'sz').values()]
-            positions = [a for a in nx.get_node_attributes(sampled_graph, 'x').values()]
-            colors_ = [a for a in nx.get_node_attributes(sampled_graph, 'colors').values()]
-            alphas = [0.1 if a=='r' else 1.0 for a in colors_]
-            sizes_ = [s*0.1 if r=='r' else s for s,r in zip(sizes_, colors_)]
-            sizes_ = [s*10.0 if r=='g' else s for s,r in zip(sizes_, colors_)]
-            # plt.figure(0)
-            # pdb.set_trace()
+            plt.figure()
             try:
                 nx.draw_networkx_nodes(sampled_graph, pos, node_size=sizes_, node_color=colors_, alpha=alphas) #pos=nx.spring_layout(sampled_graph),
             except:
@@ -128,11 +130,12 @@ for row in metadata.iterrows():
                 nx.draw_networkx_edges(sampled_graph, pos, edgelist=[edge], width=0.2*edge[2])
             plt.savefig("test_random2.png")
             print(sampled_graph.number_of_edges())
-        pdb.set_trace()
+        
+        # pdb.set_trace()
         data = GraphDataset(fname,  train_percent, num_neighbors_sampling).pyg_graph
-        fil =  open(folder+fname+'_', 'wb')
-        pickle.dump(graph, fil)   
-        fil.close()
+        # fil =  open(folder+fname+'_', 'wb')
+        # pickle.dump(graph, fil)   
+        # fil.close()
         # break
 
     #     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -181,10 +184,13 @@ for row in metadata.iterrows():
         # plt.figure()
         # fig2 = px.scatter(x=two_d2[:,0], y=two_d2[:,1], size=weights)
         # fig2.write_html('original_'+str(row[1][1]) + str(row[1][2]) + str(row[1][3])+'.html')
-        plt.figure()
-        plt.scatter(two_d2[:, 0], two_d2[:, 1],  s=weights, alpha=0.4)
+        # plt.figure()
+        import plotly.express as px
+        fig = px.scatter(x=two_d2[:, 0], y=two_d2[:, 1],  size=sizes_, opacity=alphas, color=colors_)
+        
         # # plt.savefig('./figures/original_'+str(row[1][1]) + str(row[1][2]) + str(row[1][3])+'.fig')
-        plt.savefig('original_'+str(row[1][1]) + str(row[1][2]) + str(row[1][3])+'.png')
+        fig.write_html('original_'+str(row[1][1]) + str(row[1][2]) + str(row[1][3])+'.html')
+        # plt.savefig('original_'+str(row[1][1]) + str(row[1][2]) + str(row[1][3])+'.png')
 
         x=tuple(data.edge_index.numpy())
         start = x[0]
@@ -204,13 +210,17 @@ for row in metadata.iterrows():
                 lenline = np.sqrt((line[0][0] - line[1][0])**2 + (line[0][1] - line[1][1])**2)
                 if lenline > 1:
                     # print(line)
-                    lines2.append(line)
+                    lines2.append(np.array([line[0][0], line[1][0], line[0][1], line[1][1], line[2]]))
 
                 # print('len line:', np.sqrt((line[0][0] - line[1][0])**2 + (line[0][1] - line[1][1])**2))
         print(len(lines2))
         # pdb.set_trace()
+        # lines3 = np.array(lines2[:9946])
         for line in lines2:
-            plt.plot([line[0][0],line[1][0]],[line[0][1], line[1][1]], 'r', linewidth=line[2]/10.0, alpha=0.4)
+            # pdb.set_trace()
+            fig.add_trace(px.line(line[:2], line[2:4], width=10+line[4]).data[0])
+        # fig.update_traces(opacity=.2)
+            # plt.plot([line[0][0],line[1][0]],[line[0][1], line[1][1]], 'r', linewidth=line[2]/10.0, alpha=0.4)
         # plt.show()
         # g2 = nx.Graph()
         # g2.add_nodes_from(two_d2[0])
@@ -223,5 +233,6 @@ for row in metadata.iterrows():
         # for edge in g2.edges(data='weight'):
         #     # pdb.set_trace()
         #     nx.draw_networkx_edges(g2, pos2, edgelist=[edge], width=edge[2])
-        plt.savefig("with_edges.png")
+        fig.write_html('with_edges_original_'+str(row[1][1]) + str(row[1][2]) + str(row[1][3])+'.html')
+        # plt.savefig("with_edges.png")
         pdb.set_trace()
